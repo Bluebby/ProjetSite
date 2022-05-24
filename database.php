4ef994a492e6db1cf9d82069baf1fbe3e91e35cf
Math78650 <pereiramatheo78@gmail.com>
2022-05-24 18:58:53 +0200

    root

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
     define('HOST','localhost');
     define('DB_NAME','Projet');
     define('USER','root');
-    define('PASS','root');
+    define('PASS','');
     
     try{
         $db = new PDO("mysql:host=" . HOST . ";dbname=" . DB_NAME, USER, PASS);
