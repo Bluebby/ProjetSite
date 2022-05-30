b9dc4669a13d8932bb9ca9501ce836f5d053bc68
Math78650 <pereiramatheo78@gmail.com>
2022-05-30 22:15:09 +0200

    derniers commentaires

--- database.php
+++ database.php
@@ -1,13 +1,14 @@
 <?php
-
+//on définit les différents parametre de connexion
 define('HOST', 'localhost');
 define('DB_NAME', 'Projet');
 define('USER', 'root');
-define('PASS', 'root');
+define('PASS', '');
 
 try {
+    //on se connecte à la base de donnée
     $db = new PDO("mysql:host=" . HOST . ";dbname=" . DB_NAME, USER, PASS);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
+} catch (PDOException $e) { //en cas d'échec on retourne e
     echo $e;
 }
